24e8b62304f5736edb79c5a0b2e0a569ff869aac
George Ang <gnap.an@gmail.com>
2018-06-15 20:09:45 +0800

    fix syntaqx.

--- src/ngx_stream_lua_req_preread.c
+++ src/ngx_stream_lua_req_preread.c
@@ -160,7 +160,7 @@ ngx_stream_lua_req_preread_handler(ngx_stream_lua_request_t *r)
 
     preread = (size_t)ngx_buf_size(r->connection->buffer);
 
-    ngx_log_debug2(NGX_LOG_DEBUG_STREAM, r->connection->log, 0,
+    ngx_log_debug1(NGX_LOG_DEBUG_STREAM, r->connection->log, 0,
                    "preread buffer filed %d", preread);
 
 }
